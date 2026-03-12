5a2088ceae74988dd07ed790a0a001a35bc12477
Kanit <stu33074@gmail.com>
2026-03-12 22:23:13 +0700

    fix: Vercel Deploy by adding TopMenuItemProps

--- src/components/TopMenuItem.tsx
+++ src/components/TopMenuItem.tsx
@@ -1,5 +1,11 @@
 import Link from "next/link";
 
+interface TopMenuItemProps {
+    title: string;
+    pageRef: string;
+}
+
+
 export default function TopMenuItem({ title, pageRef }: TopMenuItemProps) {
     return (
         <Link
